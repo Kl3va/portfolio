d923a246b70cff6c9c3ff460bb8e63120ba73905
Kl3va <ikwunzekelvin@gmail.com>
2023-09-20 16:53:44 +0100

    feat: resume page, fully responsive

--- components/Resume/Resume.tsx
+++ components/Resume/Resume.tsx
@@ -1,37 +1,58 @@
 import React from 'react'
 import { resumeData } from 'data/resumeData'
 
+//Styling
+import styles from 'components/Resume/resume.module.scss'
+
 type ResumeProps = typeof resumeData
 
 const ResumeTemplate = ({ heading, title, subHeading, jobs }: ResumeProps) => {
   return (
     <main>
-      <section>
-        <div>
-          <h1>{heading}</h1>
-          <p>{title}</p>
-        </div>
-
-        <div>
-          <h2>{subHeading}</h2>
+      <section className={styles.section}>
+        <div className={styles.resume_container}>
           <div>
-            {jobs.map((job, index) => {
-              return (
-                <div key={index}>
-                  <h3>{job.company}</h3>
-                  <div>
-                    {job.board.map((about, jobIndex) => {
+            <h1>{heading}</h1>
+            <p>{title}</p>
+          </div>
+
+          <div>
+            <h2>{subHeading}</h2>
+            <div className={styles.jobs_container}>
+              {jobs.map((job, index) => {
+                return (
+                  <div key={index} className={styles.unique_job}>
+                    <h3>{job.company}</h3>
+                    <div className={styles.jobs_table}>
+                      <div>{job.board.label}</div>
+                      <div>{job.board.role}</div>
+                      <div>{job.board.label2}</div>
+                      <div>{job.board.timeline}</div>
+                      <div>{job.board.label3}</div>
+                      <div>{job.board.description}</div>
+                    </div>
+                    {job.role && (
+                      <div>
+                        <h4>{`${job.role?.label}:`}</h4>
+                        <p>{job.role?.description}</p>
+                      </div>
+                    )}
+                    {job.others?.map((other, otherIndex) => {
                       return (
-                        <div key={jobIndex}>
-                          <p>{about.label}</p>
-                          <p>{about.description}</p>
+                        <div key={otherIndex}>
+                          <h4>{other.label}:</h4>
+                          <ul className={styles.lists}>
+                            {other.lists.map((list, listIndex) => (
+                              <li key={listIndex}>{list}</li>
+                            ))}
+                          </ul>
                         </div>
                       )
                     })}
                   </div>
-                </div>
-              )
-            })}
+                )
+              })}
+            </div>
           </div>
         </div>
       </section>
--- data/resumeData.ts
+++ data/resumeData.ts
@@ -9,20 +9,14 @@ export const resumeData = {
         label: 'Job Role',
         description: `As a frontend engineer, my focus is on crafting user-friendly and visually appealing interfaces. Also, making sure that the front-end of our applications and client's are both functional and aesthetically pleasing.`,
       },
-      board: [
-        {
-          label: 'Position',
-          description: 'Frontend Engineer',
-        },
-        {
-          label: 'Duration',
-          description: 'May 2023 - Present',
-        },
-        {
-          label: 'Description',
-          description: `Build and Maintain the company' sites/applications, Build out client projects... Focusing on the blockchain industry.`,
-        },
-      ],
+      board: {
+        label: 'Position',
+        role: 'Frontend Engineer',
+        label2: 'Duration',
+        timeline: 'May 2023 - Present',
+        label3: 'Description',
+        description: `Build and Maintain the company' sites/applications, Build out client projects...Focusing on the blockchain industry.`,
+      },
     },
 
     {
@@ -47,38 +41,26 @@ export const resumeData = {
           lists: ['DBforge, MobaXterm, Otrs, Confluence, ReactJS, Jira'],
         },
       ],
-      board: [
-        {
-          label: 'Position',
-          description: 'Software Developer Intern',
-        },
-        {
-          label: 'Duration',
-          description: 'Sept 2022 - April 2023',
-        },
-        {
-          label: 'Description',
-          description: `Design, Build and Maintain existing applications, ensuring they're highly performant to serve customers.`,
-        },
-      ],
+      board: {
+        label: 'Position',
+        role: 'Software Developer Intern',
+        label2: 'Duration',
+        timeline: 'Sept 2022 - April 2023',
+        label3: 'Description',
+        description: `Design, Build and Maintain existing applications, ensuring they're highly performant to serve customers.`,
+      },
     },
 
     {
       company: 'Applyforme',
-      board: [
-        {
-          label: 'Position',
-          description: 'Frontend Developer',
-        },
-        {
-          label: 'Duration',
-          description: 'Oct 2022 - Dec 2022',
-        },
-        {
-          label: 'Description',
-          description: `Build company's website and application(user dashboard, checkout)`,
-        },
-      ],
+      board: {
+        label: 'Position',
+        role: 'Frontend Developer',
+        label2: 'Duration',
+        timeline: 'Oct 2022 - Dec 2022',
+        label3: 'Description',
+        description: `Build company's website and application(user dashboard, checkout)`,
+      },
     },
   ],
 }
