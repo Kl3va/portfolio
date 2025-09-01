6c102d5a4f0a7c5285aeb9f7daf6c8f6ced21c1d
Ikwunze Kelvin <ikwunzekelvin@gmail.com>
2025-09-01 17:03:13 +0100

    updated: adding new responsibility

--- data/resumeData.ts
+++ data/resumeData.ts
@@ -1,66 +1,99 @@
 export const resumeData = {
-  heading: 'Resume',
+  heading: "Resume",
   title: `I'm passionate about crafting remarkable user experiences that prioritize speed, robustness, and pixel-perfection in responsiveness. My acute attention to detail drives me to consistently deliver excellence and exceed expectations.`,
-  subHeading: 'Experience',
+  subHeading: "Experience",
   jobs: [
     {
-      company: 'Lunarstack',
+      company: "StridePass",
       role: {
-        label: 'Job Role',
-        description: `As a frontend engineer, my focus is on crafting user-friendly and visually appealing interfaces. Also, making sure that the front-end of our applications and client's are both functional and aesthetically pleasing.`,
+        label: "Job Role",
+        description: `My role as a Frontend developer included designing and building company products and landing pages.`,
+      },
+      others: [
+        {
+          label: "Responsibilities",
+          lists: [
+            "Rebuilt and launched company's waitlist page. Improving page responsiveness and reducing load times by 40%, resulting in a 15% increase in early user sign-up conversions.",
+            "Led end-to-end development of a gamified Ambassador Program application, creating a key acquisition channel that attracted 50+ ambassadors in the first day",
+            "Engineered a dynamic dashboard for tracking referrals, tasks, and rewards, enabling users to earn and redeem points via integrated reward system, driving a 21% increase in referral-driven traffic.",
+            "Optimized Core Web Vitals across the platform, achieving scores of INP: 16 ms, CLS: 0.01, and LCP: 1.79 s to ensure fast, smooth, and high-performing user experience across all devices.",
+            "Implemented accessibility and SEO best practices on the waitlist page, enhancing Lighthouse accessibility score from 80 to 92 and performance score from 37 to 76, boosting user experience and discoverability.",
+          ],
+        },
+        {
+          label: "Technologies worked with",
+          lists: [
+            "NextJS, TailwindCSS, axios, React-Hook-Form, Recharts, zod,",
+          ],
+        },
+      ],
+      board: {
+        label: "Position",
+        role: "Frontend Engineer (Contract)",
+        label2: "Duration",
+        timeline: "May 2025 - Present",
+        label3: "Description",
+        description: `Build and Maintain the agency's sites/applications, Build out client projects...Focusing on the blockchain industry.`,
+      },
+    },
+    {
+      company: "Lunarstack",
+      role: {
+        label: "Job Role",
+        description: `As a frontend engineer, my focus was on crafting user-friendly and visually appealing interfaces. Also, making sure that the front-end of our applications and client's are both functional and aesthetically pleasing.`,
       },
       board: {
-        label: 'Position',
-        role: 'Frontend Engineer',
-        label2: 'Duration',
-        timeline: 'May 2023 - Present',
-        label3: 'Description',
+        label: "Position",
+        role: "Frontend Engineer",
+        label2: "Duration",
+        timeline: "June 2023 - March 2025",
+        label3: "Description",
         description: `Build and Maintain the company' sites/applications, Build out client projects...Focusing on the blockchain industry.`,
       },
     },
 
     {
-      company: 'IPNX',
+      company: "IPNX",
       role: {
-        label: 'Job Role',
+        label: "Job Role",
         description: `As a Software Engineer, I efficiently maintained software systems that underpinned the company's infrastructure. My duties included documenting all incidents that occurred, along with their corresponding resolutions, add new features to applications.`,
       },
       others: [
         {
-          label: 'Responsibilities',
+          label: "Responsibilities",
           lists: [
-            'Manage / Maintain assigned software applications',
-            'Work close with the System Operations team in terms of incident resolutions.',
-            'Document all new integrations and resolutions.',
-            'Add new user interface features',
-            'TroubleShoot and Maintain POS terminals',
+            "Manage / Maintain assigned software applications",
+            "Work close with the System Operations team in terms of incident resolutions.",
+            "Document all new integrations and resolutions.",
+            "Add new user interface features",
+            "TroubleShoot and Maintain POS terminals",
           ],
         },
         {
-          label: 'Technologies worked with',
-          lists: ['DBforge, MobaXterm, Otrs, Confluence, ReactJS, Jira'],
+          label: "Technologies worked with",
+          lists: ["DBforge, MobaXterm, Otrs, Confluence, ReactJS, Jira"],
         },
       ],
       board: {
-        label: 'Position',
-        role: 'Software Developer',
-        label2: 'Duration',
-        timeline: 'Sept 2022 - April 2023',
-        label3: 'Description',
+        label: "Position",
+        role: "Software Developer",
+        label2: "Duration",
+        timeline: "Sept 2022 - April 2023",
+        label3: "Description",
         description: `Design, Build and Maintain existing applications, ensuring they're highly performant to serve customers.`,
       },
     },
 
     {
-      company: 'Applyforme',
+      company: "Applyforme",
       board: {
-        label: 'Position',
-        role: 'Frontend Developer',
-        label2: 'Duration',
-        timeline: 'Oct 2022 - Dec 2022',
-        label3: 'Description',
+        label: "Position",
+        role: "Frontend Developer",
+        label2: "Duration",
+        timeline: "Oct 2022 - Dec 2022",
+        label3: "Description",
         description: `Build company's website and application(user dashboard, checkout)`,
       },
     },
   ],
-}
+};
