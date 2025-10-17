2379a6ed89a85c24eaa8b3014908f349f2e698d4
Ikwunze Kelvin <ikwunzekelvin@gmail.com>
2025-10-17 20:42:35 +0100

    update: change twitter profile

--- data/homeData.ts
+++ data/homeData.ts
@@ -1,29 +1,29 @@
-import profileImg from 'public/IMG_0082 (1).png'
+import profileImg from "public/IMG_0082 (1).png";
 
 export const homeData = {
   photo: profileImg,
-  names: 'Ikwunze Kelvin',
-  role: 'Frontend Engineer',
+  names: "Ikwunze Kelvin",
+  role: "Frontend Engineer",
   links: [
     {
-      url: 'https://github.com/Kl3va',
-      logo: 'fa-brands fa-github',
-      label: 'Visit my GitHub profile',
+      url: "https://github.com/Kl3va",
+      logo: "fa-brands fa-github",
+      label: "Visit my GitHub profile",
     },
     {
-      url: 'https://twitter.com/_Kl3va_',
-      logo: 'fa-brands fa-x-twitter',
-      label: 'Visit my twitter profile',
+      url: "https://twitter.com/i___nze",
+      logo: "fa-brands fa-x-twitter",
+      label: "Visit my twitter profile",
     },
     {
-      url: 'https://www.linkedIn.com/in/ikwunze-kelvin-3a9957163',
-      logo: 'fa-brands fa-linkedin',
-      label: 'Visit my linkedIn profile',
+      url: "https://www.linkedIn.com/in/ikwunze-kelvin-3a9957163",
+      logo: "fa-brands fa-linkedin",
+      label: "Visit my linkedIn profile",
     },
     {
-      url: 'mailto:ikwunzekelvin@gmail.com',
-      logo: 'fa-solid fa-at',
-      label: 'contact me through my mail',
+      url: "mailto:ikwunzekelvin@gmail.com",
+      logo: "fa-solid fa-at",
+      label: "contact me through my mail",
     },
   ],
-}
+};
